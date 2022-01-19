d1981febeef13c0b77cd2d9c5d0c8e5e4bed1eaf
Adam Krivka <akrivka@caltech.edu>
2022-01-19 15:20:05 -0800

    went back

--- src/clojure_ui_course/assignments/lab01.cljs
+++ src/clojure_ui_course/assignments/lab01.cljs
@@ -1,12 +1,18 @@
+(ns clojure-ui-course.assignments.lab01
+  (:require [clojure-ui-course.shared.components :as c]))
+
+(defn main []
+  [:div
+   (c/md "
 # Lab01: Basic Clojure
 
 _To Clojure, or to ClojureScript, that is the question._
 
 Clojure compiles to JVM (Java Virtual Machine) bytecode, ClojureScript compiles to JavaScript. In this course, we're going to be using exclusively ClojureScript (cljs), either in `node.js` (local JavaScript runtime) or in the browser.
 
-Because the browser tooling can get a bit complicated, we're going to start on our local machines, using `node.js`. The best way to use cljs with `node.js` for small projects is `nbb`, a small fast cljs compiler. Once installed, typing `nbb` to the terminal will spawn a cljs console similar to typing `python`."
+Because the browser tooling can get a bit complicated, we're going to start on our local machines, using `node.js`. The best way to use cljs with `node.js` for small projects is `nbb`, a small fast cljs compiler. Once installed, typing `nbb` to the terminal will spawn a cljs console similar to typing `python`.
 
-We're going to be using VS Code with the Calva extension, which provides everything we'll need (syntax highlighting, formatting, and the REPL)."
+We're going to be using VS Code with the Calva extension, which provides everything we'll need (syntax highlighting, formatting, and the REPL).
 
 ## Setup
 ### VS Code
@@ -20,7 +26,7 @@ Follow these steps:
     - a. Check by running `node -v` and `npm -v`.
 2. Install nbb by running `npm install -g nbb`.
     - a. Check by running `nbb`. 
-    - b. You're likely going to run into a permission error. [Here](https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally) is how to fix it."
+    - b. You're likely going to run into a permission error. [Here](https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally) is how to fix it.
 
 ### Caltech Gitlab SSH key
 
@@ -32,8 +38,8 @@ Follow these steps:
 5. Click 'Add key'
 
 Finally, clone the repository for this lab!
-[link]("https://gitlab.caltech.edu/cs12-clojureui-22sp/lab01")
+[link](https://gitlab.caltech.edu/cs12-clojureui-22sp/lab01)
 
 ## Part 1
 
-## Part 2
+## Part 2")])
--- src/clojure_ui_course/assignments/lab01/lab01.cljs
+++
@@ -1,7 +0,0 @@
-(ns clojure-ui-course.assignments.lab01.lab01
-  (:require [clojure-ui-course.shared.components :as c])
-  (:require-macros [clojure-ui-course.util :refer [inline-resource]]))
-
-(defn main []
-  [:div
-   (c/md (inline-resource "src/clojure_ui_course/assignments/lab01/lab01.md"))])
